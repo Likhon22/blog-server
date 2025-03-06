6e46c912ae5c4a31938fd3c11b065b15b5fba1cf
Likhon22 <likhonsarker793@gmail.com>
2025-03-06 15:06:31 +0600

    deleted unused things

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
-import { Server } from 'http';
+
 import { app } from './app';
 import config from './app/config';
-let server: Server;
+
 const port = process.env.PORT || 5000;
 async function main() {
   try {
     await mongoose.connect(config.db_url as string);
     console.log('Connected to MongoDB');
-    server = app.listen(port, () => {
+    app.listen(port, () => {
       console.log(` Server is running on port ${port}`);
     });
   } catch (err) {
