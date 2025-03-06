43feb6a54f7ce990d8e90d6a63cb43c8af4af63a
Likhon22 <likhonsarker793@gmail.com>
2025-03-06 15:05:18 +0600

    added types

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -3,7 +3,6 @@ import path from 'path';
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
-  port: process.env.PORT,
   node_env: process.env.NODE_ENV,
   db_url: process.env.DB_URL,
 };
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import { Server } from 'http';
 import { app } from './app';
 import config from './app/config';
 let server: Server;
+const port = process.env.PORT || 5000;
 async function main() {
   try {
     await mongoose.connect(config.db_url as string);
     console.log('Connected to MongoDB');
-    server = app.listen(config.port, () => {
-      console.log(` Server is running on port ${config.port}`);
+    server = app.listen(port, () => {
+      console.log(` Server is running on port ${port}`);
     });
   } catch (err) {
     console.log(err);
